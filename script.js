833f85c68aa98b5692094948ce5efbc1461e4d4d
danielfekede1616@gmail.com <danielfekede1616@gmail.com>
2025-05-25 00:44:15 +0300

    added script js file

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.querySelectorAll('nav ul li a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        const href = this.getAttribute('href');
+        if (href.startsWith('#')) {
+            e.preventDefault();
+            document.querySelector(href).scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
